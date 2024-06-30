85b24666b6a8af4ca8cae716c48084e729617400
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-06-30 16:38:57 -0600

    Se agrega la logica de nivel solo queda arreglar el menu, los iconos

--- app/src/main/java/com/example/practicaherramientas/Fragment_nivel.java
+++ app/src/main/java/com/example/practicaherramientas/Fragment_nivel.java
@@ -1,5 +1,9 @@
 package com.example.practicaherramientas;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,36 +11,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment_nivel#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_nivel extends Fragment {
+public class Fragment_nivel extends Fragment implements SensorEventListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    View vista;
+    TextView tv_z, tv_y, tv_x;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    ImageButton nivel1, nivel2;
 
     public Fragment_nivel() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_nivel.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_nivel newInstance(String param1, String param2) {
         Fragment_nivel fragment = new Fragment_nivel();
         Bundle args = new Bundle();
@@ -59,6 +59,78 @@ public class Fragment_nivel extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nivel, container, false);
+        vista = inflater.inflate(R.layout.fragment_nivel, container, false);
+
+        tv_x = vista.findViewById(R.id.tv_x);
+        tv_y = vista.findViewById(R.id.tv_y);
+        tv_z = vista.findViewById(R.id.tv_z);
+        nivel1 = vista.findViewById(R.id.nivel1);
+        nivel2 = vista.findViewById(R.id.nivel2);
+
+        sensorManager = (SensorManager) getActivity().getSystemService(getActivity().SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        nivel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSensors();
+            }
+        });
+
+        nivel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopSensors();
+            }
+        });
+
+        return vista;
+    }
+
+    private void startSensors() {
+        nivel2.setVisibility(View.VISIBLE);
+        nivel1.setVisibility(View.GONE);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    private void stopSensors() {
+        nivel2.setVisibility(View.GONE);
+        nivel1.setVisibility(View.VISIBLE);
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            double pitch = Math.atan2(-x, Math.sqrt(y * y + z * z)) * 180 / Math.PI;
+            double roll = Math.atan2(y, z) * 180 / Math.PI;
+
+            tv_x.setText(String.format("Y : %.2f°", pitch));
+            tv_y.setText(String.format("X : %.2f°", roll));
+            tv_z.setText(String.format("X: %.2f, Y: %.2f, Z: %.2f", x, y, z));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopSensors();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startSensors();
     }
 }
